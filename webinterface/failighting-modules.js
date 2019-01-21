95c02247957335ff57eb3a81b42637bb0a51ff7a
total1ty <pcmaster1337@gmail.com>
2019-01-21 21:38:55 +0100

    scene module - fade implementation

--- webinterface/failighting-modules.js
+++ webinterface/failighting-modules.js
@@ -113,28 +113,66 @@ class failScene {
   constructor(scenedata) {
     this.name = scenedata.name;
     this.group = scenedata.group;
-    this.devicevalues = scenedata.devices;
+    this.scenevalues = scenedata.devices;
+
+    if (typeof(scenedata.fade) !== 'undefined') {
+      this.fadeduration = scenedata.fade;
+    }
+    else {
+      this.fadeduration = config.fade_default;
+    }
 
     //gather involved devices
     this.scenedevices = {};
-    for (let i=0; i<this.devicevalues.length; i++) {
+    for (let i=0; i<this.scenevalues.length; i++) {
         for (let j=0; j<devices.length; j++) {
-          if (this.devicevalues[i].name == devices[j].name) {
+          if (this.scenevalues[i].name == devices[j].name) {
             this.scenedevices[i] = devices[j];
           }
         }
     }
   };
 
-  trigger() {
-    for (let i=0; i<this.devicevalues.length; i++) {
-      if(this.scenedevices[i].values.length == this.devicevalues[i].values.length) {
-        this.scenedevices[i].values = this.devicevalues[i].values;
-        this.scenedevices[i].sendValues();
+  async trigger() {
+    //return if there's already another fade currently running
+    if (scenefading) {
+      return false;
+    };
+
+    //call fade per device
+    for (let i=0; i<this.scenevalues.length; i++) {
+      if(this.scenedevices[i].values.length == this.scenevalues[i].values.length) {
+        this.fade(this.scenedevices[i], this.scenevalues[i], this.fadeduration);
       }
       else {
         console.log("SCENE: "+this.name+" - DEVICE: "+this.scenedevices[i].name+" - ERROR: tried to set keyframe with non-matching value count. ignoring device. you can ignore this too if you dont care.")
       }
     }
+  };
+
+  async fade(device, scene, duration) {
+    //calculate frame count
+    let frames = Math.floor(duration * config.max_fps/1000);
+
+    //instantly set new value if values are identical or fade length <= 1 frame
+    //TODO: The first condition always returns false, no idea why. This has worked at some point in the past, but broke somehow.
+    if ((device.values.concat() == scene.values.concat()) || (frames<=1)) {
+      device.values = scene.values.concat();
+      device.sendValues();
+    }
+
+    //fade!
+    else {
+      scenefading = true;
+      let startvalues = device.values.concat();
+      for (let i=0; i<frames; i++) {
+        for (let j=0; j<device.values.length; j++) {
+          device.values[j] = einterp(startvalues[j], scene.values[j], (i+1)/frames);
+        };
+        device.sendValues();
+        await sleep(1000/config.max_fps);
+      }
+    }
+    scenefading = false;
   };
 };
